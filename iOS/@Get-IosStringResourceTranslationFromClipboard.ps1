d3461a27a4bee7d9f53e306b30b290c3713a8a2c
Elián Fabián <elian.fabian.el.belaizi@gmail.com>
2023-04-22 03:40:06 +0200

    Implemented iOS scripts for translations, except the one to directly translate to string resource files

---
+++ iOS/@Get-IosStringResourceTranslationFromClipboard.ps1
@@ -0,0 +1,55 @@
+Import-Module -Name "$PSScriptRoot/../Modules/GenericTranslation"
+
+
+
+$Params =
+@{
+    InputObject = (Get-Clipboard -Raw)
+    
+    SourceLanguage = 'English'
+    TargetLanguage =
+    @(
+        'Catalan',
+        'German',
+        'Spanish',
+        'French',
+        'Italian',
+        'Portuguese',
+        'Swedish'
+    )
+    ItemPattern = '"(.+)" = "(.+)";'
+}
+
+$DecodeMap = 
+@{
+    "\'" = "'"
+    '\"' = "'"
+    "\\" = "\"
+    "\n" = [System.Environment]::NewLine
+}
+
+
+return Invoke-ItemTranslation @Params `
+    -OnGetItem { $name, $content = $args
+
+        $decodedContent = Convert-String $content -Mode Decode -DecodeMap $DecodeMap
+
+        [pscustomobject]@{
+            Name = $name
+            Content = $decodedContent
+        }
+    } `
+    -OnTranslateItem { $item, $source, $target = $args
+
+        $translatedContent = Invoke-StringTranslation `
+            -InputObject $item.Content `
+            -SourceLanguage $source `
+            -TargetLanguage $target
+
+        $encodedTranslatedContent = Convert-String $translatedContent -Mode Encode -DecodeMap $DecodeMap
+
+        [pscustomobject]@{
+            Name = $item.Name
+            TranslatedContent = $encodedTranslatedContent
+        }
+    }
---
+++ iOS/SaveTo-TextFile_TranslatedIosStringFromClipboard.ps1
@@ -0,0 +1,47 @@
+Write-Host "Reading from clipboard...`n" -ForegroundColor Green
+
+
+$translationsPerLanguage = .\@Get-IosStringResourceTranslationFromClipboard.ps1
+
+if ($null -eq $translationsPerLanguage)
+{
+    Clear-Host
+
+    Write-Host "Couldn't find any string resource in your clipboard.`n" -ForegroundColor Red
+    Write-Host "Your clipboard content:`n" -ForegroundColor Green
+    Write-Host (Get-Clipboard -Raw)
+
+    # Remain console open
+    while ($true)
+    {
+        Read-Host
+    }
+}
+
+$translatedStringResourceSb = [System.Text.StringBuilder]::new()
+
+foreach ($data in $translationsPerLanguage)
+{
+    $translatedStringResourceSb.Append("/* $($data.LanguageName) */`n") > $null
+
+    $listOfTranslationByName = $data.Translations
+
+    foreach ($stringResourceData in $listOfTranslationByName)
+    {
+        $translatedStringResourceSb.Append("""$($stringResourceData.Name)"" = ""$($stringResourceData.TranslatedContent)"";`n") > $null
+    }
+
+    $translatedStringResourceSb.Append("`n") > $null
+}
+
+Clear-Host
+Write-Host $translatedStringResourceSb.ToString()
+
+New-Item -Name "Translations" -ItemType Directory -ErrorAction Ignore
+New-Item -Path "Translations/strings $(Get-Date -Format 'yyyy-MM-dd hh;mm;ss').txt" -Value $translatedStringResourceSb.ToString()
+
+# Remain console open
+while ($true)
+{
+    Read-Host
+}
