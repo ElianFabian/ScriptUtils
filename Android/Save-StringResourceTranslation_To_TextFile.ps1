a9e4904a6a552d5232e7906a80653ea4a4e2b9c1
Elián Fabián <elian.fabian.el.belaizi@gmail.com>
2024-01-28 15:20:46 +0100

    Update Save-StringResourceTranslation_To_TextFile.ps1

--- Android/Save-StringResourceTranslation_To_TextFile.ps1
+++ Android/Save-StringResourceTranslation_To_TextFile.ps1
@@ -15,8 +15,34 @@ foreach ($data in $translationsPerLanguageWithFormat)
 Clear-Host
 Write-Host $translatedStringResourceSb.ToString()
 
+$fileName = "strings $(Get-Date -Format 'yyyy-MM-dd hh;mm;ss').txt"
+$filePath = Join-Path -Path $PSScriptRoot, "Translations" -ChildPath $fileName
 
-New-Item -Path "Translations/strings $(Get-Date -Format 'yyyy-MM-dd hh;mm;ss').txt" -Value $translatedStringResourceSb.ToString() -Force
+New-Item -Path $filePath -Value $translatedStringResourceSb.ToString() -Force
+
+
+# TODO: Find a way to show a notification cross-platform (Windows, Linux, macOS)
+# function Show-Notification
+# {
+#     param
+#     (
+#         [string] $Title = "Title",
+#         [string] $Text = "Some text"
+#     )
+
+#     Add-Type -AssemblyName System.Windows.Forms
+
+#     $notif = New-Object System.Windows.Forms.NotifyIcon
+#     $path = (Get-Process -Id $PID).Path
+#     $notif.Icon = [System.Drawing.Icon]::ExtractAssociatedIcon($path)
+#     $notif.BalloonTipIcon = [System.Windows.Forms.ToolTipIcon]::Info
+#     $notif.BalloonTipText = $Text
+#     $notif.BalloonTipTitle = $Title
+#     $notif.Visible = $true
+#     $notif.ShowBalloonTip(0)
+# }
+
+# Show-Notification -Title "Translations finished" -Text "Path: $filePath"
 
 
 while ($true)
