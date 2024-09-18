7a26fd361c566ac3e2b92b84d66f58f4e4b13eac
Elián Fabián <elian.fabian.2001@gmail.com>
2024-09-18 16:21:09 +0200

    Allow environment change  befor calling an endpoint

--- Android/Automation/VolaApis.psm1
+++ Android/Automation/VolaApis.psm1
@@ -1,14 +1,45 @@
 $ApiKey = $env:PmApiKey
 
-$Domain = "vola.plus"
+
+$script:Environment = "Production"
+
+function Set-VolaEnvironment {
+
+    param (
+        [Parameter(Mandatory)]
+        [ValidateSet("Production", "Test")]
+        [string] $Environment
+    )
+
+    $script:Environment = $Environment
+}
+
+function Get-VolaEnvironment {
+    return $script:Environment
+}
+
+function GetVolaDomain {
+    switch ($script:Environment) {
+        "Production" {
+            return "vola.plus"
+        }
+        "Test" {
+            return "test.vola.plus"
+        }
+        default {
+            Write-Error "Unknown environment: $script:Environment"
+        }
+    }
+}
 
 function Invoke-ApiUserExists([string] $Email) {
     try {
-        $response = Invoke-RestMethod -Method Get -Uri "https://$Domain/api/v1/login?email=$([uri]::EscapeDataString($Email))&password=0&api_key=$ApiKey&lang=en"
+        $domain = GetVolaDomain
+        $response = Invoke-RestMethod -Method Get -Uri "https://$domain/api/v1/login?email=$([uri]::EscapeDataString($Email))&password=0&api_key=$ApiKey&lang=en"
         $message = [string] $response.message
-    
+
         switch ($message) {
-            "Incorrect password" { 
+            "Incorrect password" {
                 return $true
             }
             "User not found" {
@@ -27,7 +58,8 @@ function Invoke-ApiUserExists([string] $Email) {
 
 function Invoke-ApiNationalities() {
     try {
-        $response = Invoke-RestMethod -Method Get -Uri "https://$Domain/api/v2/settings/nationalities?api_key=$ApiKey&lang=en"
+        $domain = GetVolaDomain
+        $response = Invoke-RestMethod -Method Get -Uri "https://$domain/api/v2/settings/nationalities?api_key=$ApiKey&lang=en"
         return $response.data
     }
     catch {
@@ -38,7 +70,8 @@ function Invoke-ApiNationalities() {
 
 function Invoke-ApiContries() {
     try {
-        $response = Invoke-RestMethod -Method Get -Uri "https://$Domain/api/v2/settings/countries?api_key=$ApiKey&lang=en"
+        $domain = GetVolaDomain
+        $response = Invoke-RestMethod -Method Get -Uri "https://$domain/api/v2/settings/countries?api_key=$ApiKey&lang=en"
         return $response.data
     }
     catch {
